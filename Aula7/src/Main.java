3e8f0e1b9a368adeb3378dd037d7f1f09f01c8b7
LuizadeMartha <124800649+LuizadeMartha@users.noreply.github.com>
2023-04-19 14:54:31 -0300

    Update Main.java

--- Aula7/src/Main.java
+++ Aula7/src/Main.java
@@ -3,15 +3,16 @@ public class Main {
 
 
         Camiseta c1 = new Camiseta("Camiseta", 35, "Rosa", "M");
-        c1.getCor();
-        c1.getTamanho();
         c1.etiquetaPreco();
+        System.out.println(c1.getCor());
+        System.out.println(c1.getTamanho());
 
         Notebook n1 = new Notebook("Sansung",1800,"GTX", "Intel", 500);
-        n1.getGpu();
-        n1.getProcessador();
-        n1.getArmanezamento();
         n1.etiquetaPreco();
+        System.out.println(n1.getGpu());
+        System.out.println(n1.getProcessador());
+        System.out.println(n1.getArmanezamento());
+
 
 
     }
