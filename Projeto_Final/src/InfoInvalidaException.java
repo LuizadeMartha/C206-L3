5649baf700aa21efafe48105161a7dfb1840f05e
LuizadeMartha <124800649+LuizadeMartha@users.noreply.github.com>
2023-06-22 14:50:58 -0300

    Update InfoInvalidaException.java

--- Projeto_Final/src/InfoInvalidaException.java
+++ Projeto_Final/src/InfoInvalidaException.java
@@ -2,7 +2,6 @@ public class InfoInvalidaException extends Exception {
 
     public InfoInvalidaException(String message){
         super(message);
-        //System.out.println("Não é permitido o numero de pessoas digitados");
     }
 
 }
